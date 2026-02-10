8c092946e5ed1a2e9e4dd963d0003e17dbebd0c0
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-02-10 15:02:46 +0100

    add third snack

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,58 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const char = "A"
+
+function searchForFirstChar(array, firstChar) {
+
+    let charSearch = []
+
+    for (let i = 0; i < array.length; i++){
+
+        const firstLetter = array[i]
+
+        if (firstLetter.charAt(0) === firstChar)
+
+            charSearch.push(firstLetter)
+        
+    }
+
+    return charSearch
+}
+ 
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+ const result = searchForFirstChar (names, char)
+
+console.log(result);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+
+//arrow function
+
+/* const searchForFirstChar = (array, firstchar) => {
+
+
+    let charSearch = []
+
+    for (let i = 0; i < array.length; i++){
+
+        const firstLetter = array[i]
+
+        if (firstLetter.charAt(0) === firstchar)
+
+            charSearch.push(firstLetter)
+        
+    }
+
+    return charSearch
+}
+
+const result = searchForFirstChar (names, char)
+
+console.log(result); */
+
