c849dd323fb59ce1009b79b872feedd00921e750
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-02-10 15:38:52 +0100

    add 4 snack

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,52 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const vocals = ["a", "e" ,"i", "o", "u", "A", "E", "I", "O", "U"]
 
 // Dichiara la funzione qui.
+
+function checkVocalsInWord (word, vocals) {
+
+    let vocalFound = []
+    
+    for(let i = 0; i < word.length ; i ++){
+       const char = word[i]
+        if (vocals.includes(char)) {
+            vocalFound.push(char);
+        }
+    }
+    return vocalFound
+}
+
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
 
+const vocalResult = checkVocalsInWord (word, vocals)
+console.log(vocalResult);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+//arrow function
+
+/* const checkVocalsInWord = (word, vocals) => {
+        let vocalFound = []
+    
+    for(let i = 0; i < word.length ; i ++){
+       const char = word[i]
+        if (vocals.includes(char)) {
+            vocalFound.push(char);
+        }
+    }
+    return 
+
+    return vocalFound
+
+}
+
+const vocalResult = checkVocalsInWord (word, vocals)
+console.log(vocalResult);
+ */
